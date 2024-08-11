4b46c83f7c0b69c8bd1d7e0415758a9c8db82dc7
Ishan Bagchi <ishanbagchi4@gmail.com>
2024-08-11 19:48:35 +0530

    add type declaration

--- src/Button.tsx
+++ src/Button.tsx
@@ -5,8 +5,12 @@ interface Props extends React.HTMLAttributes<HTMLButtonElement> {
 	children?: React.ReactNode
 }
 
-const Button: FC<Props> = ({ children }) => {
-	return <div className={styles.button}>{children}</div>
+const Button: FC<Props> = ({ children, ...props }) => {
+	return (
+		<button className={styles.button} {...props}>
+			{children}
+		</button>
+	)
 }
 
 export default Button
